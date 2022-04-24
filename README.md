d544e7acd44fdbfb8eda322fe2facc51c737f08d
KyaDLFiles <103515463+KyaDLFiles@users.noreply.github.com>
2022-04-24 17:56:46 +0200

    1.1, parallelism!

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # kya_dl_bruteforce_checksum
-Bruteforce all possible start and ending positions of a given known checksum for a Kya: Dark Lineage save file (or possibly any Eden Games game using the same base libraries)
+Bruteforce all possible start and ending positions of a given known checksum for a Kya: Dark Lineage save file (and possibly any Eden Games game using the same base libraries)
 
 # Usage
-bfcsum(.exe) \<h>\<filesize/auto> \<h>known\_checksum \<h>start\_offset  
-filesize can be set to "auto" to have the program find out on it's own  
+bfcsum(.exe) /path/to/save/file.dat \<h\>known\_hash \<--start=\<h\>start\_offset\> \<--stop=\<h\>stop\_offset\>
 Values are read as decimal by default, prepend lowercase "h" to have one read as hexadecimal  
-The program will print the starting offset every time it's increased (meaning it has tried all combinations to start:start√+1 to start:EOF)  
-Upon finding a matching checksum, it will print the checksum, the starting offset, the ending offset, and exit
+The program will print the starting offset every time it's increased (meaning it has tried all combinations to start:start+1 to start:EOF)  
+Upon finding a matching checksum, it will print the checksum, the starting offset, the ending offset, and exit  
+## Return codes
+*0: match found*  
+*1: error opening file*  
+*2: missing argument(s)*  
+*3: program finished succesfully, but no match found*  
+*4: value(s) of argument(s) out of range*  
+*5: invalid argument(s)*  
--- main.c
+++ main.c
@@ -4,85 +4,126 @@
 #include <string.h>
 #include "crc32.h"
 
-uint64_t compute_checksum(const void *buffer, int64_t size) {
-    uint64_t result;
-    int64_t v4;
-    uint8_t v5;
-    int64_t v6;
+int get_argument_value(char arg[]) {
+	char * endptr;
+	int ret;
 
-    result = 0xFFFFFFFFFFFFFFFFLL;
-    v4 = (int)size - 1;
-    if (size) {
-        do {
-            v5 = *(uint8_t *)buffer;
-            v6 = v4;
-            v4 = (int32_t)v4 - 1;
-            buffer = (uint8_t *)buffer + 1;
-            result = ((uint32_t)result >> 8) ^ (uint64_t)crc32_tab[(uint8_t)(result ^ v5)];
-        } while ( v6 );
-    }
-    return result;
+	if (arg[0] == 'h') {
+		ret = strtol(arg + 1, &endptr, 16);
+	}
+	else {
+		ret = strtol(arg, &endptr, 10);
+	}
+
+	//In case of invalid values
+	if (endptr == arg || *endptr != '\0')
+		ret = -1;
+
+	return ret;
+}
+
+uint64_t compute_checksum(const void *buffer, int64_t size) {
+	uint64_t result;
+	int64_t v4;
+	uint8_t v5;
+	int64_t v6;
+
+	result = 0xFFFFFFFFFFFFFFFFLL;
+	v4 = (int)size - 1;
+	if (size) {
+		do {
+			v5 = *(uint8_t *)buffer;
+			v6 = v4;
+			v4 = (int32_t)v4 - 1;
+			buffer = (uint8_t *)buffer + 1;
+			result = ((uint32_t)result >> 8) ^ (uint64_t)crc32_tab[(uint8_t)(result ^ v5)];
+		} while ( v6 );
+	}
+	return result;
 }
 
 int main(int argc, char *argv[]) {
-    //Variable definitions
-    FILE * fp;
-    uint8_t *buf;
-    uint32_t csum, known_csum;
-    int i, j, fsize, start_value;
+	//Variables definition
+	FILE * fp;
+	uint8_t *buf;
+	uint32_t csum, known_csum, fsize, i, j;
+	int start_offset = 0, stop_offset, tot_read;
+	_Bool found = 0;
 
-    //Check file path argument
-    if (argc < 5) {
-        printf("Missing argument!\n"
-               "Usage: kdl_bf(.exe) /path/to/save/file.dat filesize(hex) known_csum(dec) start_value(hex)\n"
-               "filesize can be set to \"auto\" to make it automatic");
-        return 1;
-    }
+	//Check file path and hash argument
+	if (argc < 3) {
+		fprintf(stderr, "Missing argument(s)!\n"
+						"bfcsum(.exe) /path/to/save/file.dat <h>known_checksum <--start=<h>start_offset>> <--stop=<h>stop_offset>\n"
+						"Default offsets are the start and the end of the file\n"
+						"Values are read as decimal by default, prepend lowercase \"h\" to have one read as hexadecimal");
+		return 1;
+	}
 
-    //Open file
-    if ((fp = fopen(argv[1], "rb")) == NULL) {
-        printf("Error opening file!\n");
-        return 2;
-    }
+	//Open file
+	if ((fp = fopen(argv[1], "rb")) == NULL) {
+		fprintf(stderr, "Error opening file!\n");
+		return 2;
+	}
 
-    //Get file size
-    if (strcmp(argv[2], "auto") == 0) {
-        fseek(fp, 0, SEEK_END);
-        fsize = ftell(fp);
-        fseek(fp, 0, SEEK_SET);
-    } else if (argv[2][0] == 'h')
-        fsize = strtol(argv[2] + 1, NULL, 16);
-    else
-        fsize = strtol(argv[2], NULL, 10);
+	//Get file size and assign it to stop_offset as default
+	fseek(fp, 0, SEEK_END);
+	fsize = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	stop_offset = (int)fsize;
 
-    //Get known checksum
-    if (argv[3][0] == 'h')
-        known_csum = strtoul(argv[3] + 1, NULL, 16);
-    else
-        known_csum = strtoul(argv[3], NULL, 10);
+	//Reset errno and get checksum
+	known_csum = get_argument_value(argv[2]);
 
-    //Get starting position
-    if (argv[4][0] == 'h')
-        start_value = strtol(argv[4] + 1, NULL, 16);
-    else
-        start_value = strtol(argv[4], NULL, 10);
+	//Read other arguments
+	for (i = 3; i < argc; i++) {
+		if (strncmp("--start=", argv[i], 8) == 0)
+			start_offset = get_argument_value(argv[i] + 8);
+		else if (strncmp("--stop=", argv[i], 7) == 0)
+			stop_offset = get_argument_value(argv[i] + 7);
+		else {
+			fprintf(stderr, "Error: invalid argument(s)!\n");
+			return 5;
+		}
+	}
 
-    //Allocate buffer and read file
-    buf = (uint8_t*)malloc(fsize);
-    fread(buf, fsize, 1, fp);
+	//Exit if values are invalid
+	tot_read = stop_offset - start_offset;
+	if (start_offset > fsize || stop_offset > fsize || start_offset < 0 || stop_offset < 0 || tot_read <= 0 || tot_read > fsize) {
+		fprintf(stderr, "Error: invalid value(s)! Check the arguments\n");
+		if (start_offset > fsize || stop_offset > fsize)
+			fprintf(stderr, "Offset(s) larger than file size");
+		return 4;
+	}
 
-    //Start bruteforcing
-    for (i = start_value; i < fsize; i++) { //Start from specified value and increase each time
-        printf("%X\n", i); //Print current start value to let the user keep track
-        for (j = fsize - i; j > 0; j--) { //Start from right after i and increase each time
-            csum = compute_checksum(buf + i, fsize - i - j); //Calculate checksum buffer[i:-j]
-            if (csum == known_csum) { //Print and return 0 if checksum is found
-                printf("%08X! %02X:%02X", csum, i, fsize - j);
-                return 0;
-            }
-        }
-    }
+	//Allocate buffer and read file
+	buf = (uint8_t*)malloc(tot_read);
+	fseek(fp, (long)start_offset, SEEK_SET);
+	fread(buf, tot_read, 1, fp);
 
-    //Return 3 if not found
-    return 3;
+	//Start bruteforcing
+	for (i = 0; i < tot_read; i++) { //Start from specified value and increase each time
+		if (found) //Exit if previously found
+			break;
+		printf("h%X\n", i + start_offset); //Print current start value to let the user keep track
+		#pragma omp parallel for shared(found)
+		for (j = tot_read - i; j > 0; j--) { //Start from right after i and increase each time
+			if (found) //Skip if already found
+				continue;
+			csum = compute_checksum(buf + i, tot_read - i - j); //Calculate checksum buffer[i:-j]
+			if (csum == known_csum) { //Print and skip all the rest if found (omp doesn't like break)
+				printf("Match found for %08X!\n"
+					   "Start: h%02X; stop: h%02X\n",
+					   csum, i + start_offset, stop_offset - j);
+				found = 1;
+			}
+		}
+	}
+
+	//Return 3 if not found
+	if (found)
+		return 0;
+	else {
+		printf("No match found :(\n");
+		return 3;
+	}
 }
